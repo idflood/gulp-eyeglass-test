de6908636d79939d92779ede9f370085d68102c2
David Mignot <david.mig@gmail.com>
2015-05-30 08:46:53 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var eyeglass = require("eyeglass")({
+}, sass.compiler);
+
+// Disable import once with gulp until we
+// figure out how to make them work together.
+eyeglass.enableImportOnce = false;
+
+gulp.task('default', function() {
+  return gulp.src('src/*.*')
+    .pipe(sass(eyeglass.sassOptions()).on("error", sass.logError))
+    .pipe(gulp.dest('dist'));
+});
